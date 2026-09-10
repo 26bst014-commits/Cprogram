19edd1e04560c1dd09f634ac1aa2e517c586781c
26bst014-commits <26bst014@hicas.ac.in>
2026-09-10 21:43:46 +0530

    Create Float.c

---
+++ Float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float r = 5.0, area;
+    
+    area = 3.14 * r * r;
+    
+    printf("Area of circle = %.2f\n", area);
+    return 0;
+}
+output:
+       Area of circle = 78.50
